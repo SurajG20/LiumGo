f72c6577bf6f0c1fb40a4fc6afde4e829a1dfb6c
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2023-10-27 09:43:35 +0530

    few changes

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 const secret = process.env.SECRET;
+
 app.use(
   session({
     secret,
